85c9e9c48c7e8b00a71b2b999d96b26152b1340c
Khan, Farrukh <farrukh.khan@news.com.au>
2020-07-15 20:03:44 +1000

    .

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,15 +1,9 @@
-<template>
-  <article>
-    <h1>{{blogPost.title}}</h1>
-    <div>{{blogPost.body}}</div>
-  </article>
-</template>
 <script>
 export default {
   computed: {
     blogPosts() {
       return this.$store.state.blogPosts;
-    },
-  },
+    }
+  }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        netlify-cms
+        netlify-cmssss
       </h1>
       <div class="links">
         <a
