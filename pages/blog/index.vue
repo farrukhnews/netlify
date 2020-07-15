882ba57fac9497d43ffd22f38fd6d96a18351e94
Khan, Farrukh <farrukh.khan@news.com.au>
2020-07-15 20:23:24 +1000

    blog temlate

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,13 @@
+<template>
+  <section>
+    <section v-for="post in blogPosts" :key="post.date">
+      <h1 v-text="post.title"></h1>
+      <h3 v-text="post.description"></h3>
+      <p v-text="post.body"></p>
+    </section>
+  </section>
+</template>
+
 <script>
 export default {
   computed: {
