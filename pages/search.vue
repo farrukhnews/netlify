be908919a47867c2b3f84445ac2d99244a061ee4
Khan, Farrukh <farrukh.khan@news.com.au>
2020-07-22 13:44:17 +1000

    .

---
+++ pages/search.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div class="container">
+      <h1>Search Results for XXX</h1>
+
+    </div>
+  </section>
+</template>
+<script>
+
+export default {
+};
+</script>
+<style>
+.container {
+  max-width: 1024px;
+  margin: 4rem auto;
+}
+a {
+  text-decoration: none;
+  text-align: center;
+  color: #333;
+  text-transform: uppercase;
+}
+h1 {
+  text-align: center;
+  font-size: 2rem;
+}
+h3 {
+  text-align: center;
+  text-transform: uppercase;
+}
+nav {display: flex; justify-content: center; margin: 1rem auto 3rem; font-weight: 700;}
+nav a {display: block;}
+nav img {margin: 0 auto;}
+nav span {display: block; margin: 0 auto;}
+.content ul{display: flex;}
+.content .img{display: block; width: 100%; height: 11rem; background: #ccc; margin-bottom: 1rem;}
+.content ul li{ list-style: none; padding: 1rem 0.5rem; text-align: center;}
+</style>
