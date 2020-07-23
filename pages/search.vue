9bc8eed76b6d95853b8d90763ef71c25ab06cd5c
farrukhnews <farrukh.khan@news.com.au>
2020-07-23 16:27:19 +1000

    search test

--- pages/search.vue
+++ pages/search.vue
@@ -1,23 +1,29 @@
 <template>
   <section>
     <div class="container">
+      <input v-model="value" />
+      <button @click="getData(value)">submit</button>
       <h1>Search Results for XXX</h1>
-      <div class="loop" v-for="page in pageData">
-        <h2 class="data" v-text="page"></h2>
-
-      </div>
+	  <div>{{ results }}</div>
     </div>
   </section>
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  computed: {
-    pageData() {
-      let pages = this.$store.state.searchResult;
-      for (let i = 0; i < pages.length; ++i) {
-          console.log(pages[i]);
-      }
-    },
+  data() {
+    return { value: '', results: '' };
+  },
+  methods: {
+    async getData(value) {
+      const results = await fetch(`/.netlify/functions/search?search=${value}`);
+      console.log(results);
+    }
+  },
+  mounted() {
+    const response = axios.get("/searchIndex.json");
+    console.log(response);
   }
 };
 </script>
@@ -40,11 +46,35 @@ h3 {
   text-align: center;
   text-transform: uppercase;
 }
-nav {display: flex; justify-content: center; margin: 1rem auto 3rem; font-weight: 700;}
-nav a {display: block;}
-nav img {margin: 0 auto;}
-nav span {display: block; margin: 0 auto;}
-.content ul{display: flex;}
-.content .img{display: block; width: 100%; height: 11rem; background: #ccc; margin-bottom: 1rem;}
-.content ul li{ list-style: none; padding: 1rem 0.5rem; text-align: center;}
+nav {
+  display: flex;
+  justify-content: center;
+  margin: 1rem auto 3rem;
+  font-weight: 700;
+}
+nav a {
+  display: block;
+}
+nav img {
+  margin: 0 auto;
+}
+nav span {
+  display: block;
+  margin: 0 auto;
+}
+.content ul {
+  display: flex;
+}
+.content .img {
+  display: block;
+  width: 100%;
+  height: 11rem;
+  background: #ccc;
+  margin-bottom: 1rem;
+}
+.content ul li {
+  list-style: none;
+  padding: 1rem 0.5rem;
+  text-align: center;
+}
 </style>
